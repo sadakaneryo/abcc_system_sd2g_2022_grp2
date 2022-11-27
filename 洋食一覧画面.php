edec09a09f785e718a6ed552eaa7891dd613fada
sadakaneryo <2101389@s.asojuku.ac.jp>
2022-11-27 15:34:51 +0900

    update

--- 洋食一覧画面.php
+++ 洋食一覧画面.php
@@ -29,19 +29,19 @@
  
     <div class="row">
     <?php
-        $pdo = new PDO('mysql:host=mysql208.phy.lolipop.lan;dbname=LAA1417400-healthybox;charset=utf8','LAA1417400','Pass0000');
-        $sql = "SELECT * FROM bento WHERE jpn=0";
-        $selectdata = $pdo->query($sql);
-        foreach($selectdata as $row){
-            echo '<div class="col-md-4">
-                <a href="和食洋食詳細画面.php?id='.$row['bento_id'].'">
-                <img style="width: 300px; height: auto;" src="'.$row['image'].'">
-                </a>
-                <p></p>
-                <p class="card-title" style="color: gray; text-align:right">カロリー '.$row['calorie'].'kcal</p>
-                <h5 style="text-align:right">'.$row['bento_name'].'弁当　'.$row['price'].'円</h5>
-             </div>';
-        }
+      $pdo = new PDO('mysql:host=mysql208.phy.lolipop.lan;dbname=LAA1417400-healthybox;charset=utf8','LAA1417400','Pass0000');
+      $sql = "SELECT * FROM bento WHERE jpn=0";
+      $selectdata = $pdo->query($sql);
+      foreach($selectdata as $row){
+          echo '<div class="col-md-4">
+              <a href="和食洋食詳細画面.php?id='.$row['bento_id'].'">
+              <img style="width: 300px; height: auto;" src="'.$row['image'].'">
+              </a>
+              <p></p>
+              <p class="card-title" style="color: gray; text-align:right">カロリー '.$row['calorie'].'kcal</p>
+              <h5 style="text-align:right">'.$row['bento_name'].'弁当　'.$row['price'].'円</h5>
+            </div>';
+      }
     ?>
     </div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
