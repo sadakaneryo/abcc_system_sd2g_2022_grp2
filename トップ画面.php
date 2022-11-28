a36265700e8f4813df4d5aff5bb3cbf5b8acb219
sadakaneryo <2101389@s.asojuku.ac.jp>
2022-11-28 14:52:46 +0900

    Update トップ画面.php

--- トップ画面.php
+++ トップ画面.php
@@ -32,13 +32,13 @@
     <form action="検索結果一覧画面.php" method="post">
       <div class="form-group">
         <div class="row">
-          <div class="col-lg-4">
-              <img src="img/eae8ba6e-608b-4b84-b83a-a525d0429bbd.jpg"width="400">
+          <div class="col-md-4">
+              <img src="img/eae8ba6e-608b-4b84-b83a-a525d0429bbd.jpg">
           </div>
-          <div class="col-lg-7">
+          <div class="col-md-6">
               <input type="text" name="search" class="form-control" placeholder="商品名入力欄" style="width:100%;">
           </div>
-          <div class="col-lg-1">
+          <div class="col-md-2">
               <button type="submit" class= "btn">検索</button>
           </div>
         </div> 
