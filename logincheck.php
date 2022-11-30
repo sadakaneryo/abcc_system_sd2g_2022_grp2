f7ef28b319a611041fe9a5bed78f5572a2dbd19f
sadakaneryo <2101389@s.asojuku.ac.jp>
2022-11-30 10:21:45 +0900

    Update logincheck.php

--- logincheck.php
+++ logincheck.php
@@ -2,5 +2,5 @@
 require_once 'DBManager.php';
 session_start();
 $dbmng = new DBManager();
-$dbmng->loginCheck($_POST['id'],$_POST['pass']);
+$dbmng->loginCheck($_POST['userid'],$_POST['password']);
 ?>
