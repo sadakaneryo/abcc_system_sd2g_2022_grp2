da268c6b7c8fee3961a8964eb0feda5dca1a3aed
sadakaneryo <2101389@s.asojuku.ac.jp>
2022-11-30 11:27:25 +0900

    Update ユーザーログイン画面.php

--- ユーザーログイン画面.php
+++ ユーザーログイン画面.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html>
 
@@ -5,6 +8,11 @@
 <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
 <title>login</title>
 <style>
+.btn--login{
+    color: #fff;
+    background-color: #B6D7C3;
+    margin-left: auto;
+}
 </style>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -19,21 +27,23 @@
 			<div class = "card mt-5">
 				<div class = "card-body" style="background-color:rgb(143, 243, 185);">
                     <div class = "pt-3 pb-3 pl-3 pr-3">
-						<a href="トップ画面.php"><p class = "moji size big">Login</p></a>
+                        <form action="logincheck.php" method="post">
+                            <button type="submit" class = "btn btn--login"><p style="font-size: 40px; color: #62d34f;">Login</p></button>
                             <p>ユーザーⅠⅮ　　<input type = "text" name = "userid"></p>
-                            <p>パスワード　　　<input type = "text" name = "password"></p>
-					    </div>
+                            <p>パスワード　　　<input type = "password" name = "password"></p>
+                        </form>
                     </div>
-				</div>
-			</div>
-		</div>
-
-        <span>
-            <a href="新規会員登録画面.php"><h5 class = "kuro">新規会員登録</h5></a>
-        </span>
-        <div class = "ml-5 gazoichi">
-            <img class = "" src="img/c242312f152b7a5ce3fd4c26e6fd3e4c.png" alt="riceballs" title="おにぎり" width="8%" height="30%"> 
+                </div>
+            </div>
         </div>
+    </div>
+
+    <span>
+        <a href="新規会員登録画面.php"><h5 class = "kuro">新規会員登録</h5></a>
+    </span>
+    <div class = "ml-5 gazoichi">
+        <img class = "" src="img/c242312f152b7a5ce3fd4c26e6fd3e4c.png" alt="riceballs" title="おにぎり" width="8%" height="30%"> 
+    </div>
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
