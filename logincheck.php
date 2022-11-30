f4ddb6fb9b5be18bbd613f7096e3ef0797a02585
sadakaneryo <2101389@s.asojuku.ac.jp>
2022-11-30 10:35:17 +0900

    Update logincheck.php

--- logincheck.php
+++ logincheck.php
@@ -1,6 +1,5 @@
-<?php
+><?php
 session_start();
-
 $pdo = new PDO('mysql:host=mysql208.phy.lolipop.lan;dbname=LAA1417400-healthybox;charset=utf8mb4','LAA1417400','Pass0000');
 $sql = "SELECT * FROM users WHERE user_id = ?";
 
@@ -14,12 +13,16 @@ $searchArray = $ps->fetchAll();
 foreach($searchArray as $row){
     if(password_verify($_POST['password'], $row['user_password']) == true){
         $_SESSION['uid'] = $row['user_id'];
-        $_SESSION['name'] = $row['user_name'];
+        $_SESSION['name'] = $row['sei'];
 
         header('Location: トップ画面.php');
         break;
     }else{
         header('Location: ユーザーログイン画面.php');
+        break;
     }
+}
+if(count($result)==0){
+    header('Location: ユーザーログイン画面.php');
 }
 ?>
