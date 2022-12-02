c970db210b80afe279325b7a593c3e5489f4b7b4
sadakaneryo <2101389@s.asojuku.ac.jp>
2022-12-02 11:42:26 +0900

    Update logincheck.php

--- logincheck.php
+++ logincheck.php
@@ -12,9 +12,13 @@ $searchArray = $ps->fetchAll();
 
 foreach($searchArray as $row){
     if($_POST['password']==$row['user_password']){
+        $length = 7;
+        $max = pow(10, $length) - 1;                    // コードの最大値算出
+        $rand = random_int(0, $max);                    // 乱数生成
+        $code = sprintf('%0'. $length. 'd', $rand);     // 乱数の頭0埋め
         $_SESSION['uid'] = $row['user_id'];
         $_SESSION['name'] = $row['sei'];
-        $_SESSION['oid'] = 
+        $_SESSION['oid'] = $code;
 
         header('Location: トップ画面.php');
     }else{
