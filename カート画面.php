7e2a3a03a9bd6bf16d533dc29791d5f4e0e0be13
sadakaneryo <2101389@s.asojuku.ac.jp>
2022-12-04 22:20:30 +0900

    Update カート画面.php

--- カート画面.php
+++ カート画面.php
@@ -161,22 +161,22 @@
 
           
           foreach($result as $row){
-            echo "  <div class=\"col-md-8 offset-md-2 alert-success\">
-                <div class=\"row\">
-                  <div class=\"col-md-4\">
-                    <h2>$row[bento_name]</h2>
+            echo '<div class="col-md-8 offset-md-2 alert-success">
+                <div class="row">
+                  <div class="col-md-4">
+                    <h2>'.$row['bento_name'].'</h2>
                   </div>
-                  <div class=\"col-md-3\">
-                    <h2 style=\"text-align: right\">$row[kosu]</h2>
+                  <div class="col-md-3">
+                    <h2 style="text-align: right">'.$row['kosu'].'</h2>
                   </div>
-                  <div class=\"col-md-3\">
-                    <h2 style=\"text-align: center\">$row[price]*$row[kosu]</h2>
+                  <div class="col-md-3">
+                    <h2 style="text-align: center">'.$row['price']*$row['kosu'].'</h2>
                   </div>
-                  <div class=\"col-md-2\">
-                    <button onclick=\"カート削除.php?name='$row[bento_id]'\">削除</button>
+                  <div class="col-md-2">
+                    <button onclick="カート削除.php?name='.$row['bento_id'].'">削除</button>
                   </div>
                 </div>
-              </div>";
+              </div>';
           }
         ?>
       </div>
