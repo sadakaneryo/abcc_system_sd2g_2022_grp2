28295a26167efce1c8585994e52d1abecf26fbc9
sadakaneryo <2101389@s.asojuku.ac.jp>
2022-12-05 10:20:35 +0900

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
+$pdo = new PDO('mysql:host=mysql208.phy.lolipop.lan;dbname=LAA1417400-healthybox;charset=utf8','LAA1417400','Pass0000');
+$sql="DELETE FROM details";
+$ps = $pdo->query($sql);
 session_destroy();
 header('Location: ユーザーログイン画面.php');
 ?>
