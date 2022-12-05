9342540b48ddae766683cae92f808c7b9a87ca68
sadakaneryo <2101389@s.asojuku.ac.jp>
2022-12-05 10:32:11 +0900

    Update カート画面.php

--- カート画面.php
+++ カート画面.php
@@ -170,7 +170,7 @@
                     <h2 style="text-align: right">'.$row['kosu'].'</h2>
                   </div>
                   <div class="col-md-3">
-                    <h2 style="text-align: center">'.$row['price']*$row['kosu'].'円</h2>
+                    <h2 style="text-align: center">'.number_format($row['price']*$row['kosu']).'円</h2>
                   </div>
                   <div class="col-md-2">
                     <a href="カート削除.php?name='.$row['bento_id'].'">
@@ -235,7 +235,7 @@
                     <h2 style="text-align: right">'.$row['kosu'].'</h2>
                   </div>
                   <div class="col-md-3">
-                    <h2 style="text-align: center">'.$row['price']*$row['kosu'].'円</h2>
+                    <h2 style="text-align: center">'.number_format($row['price']*$row['kosu']).'円</h2>
                   </div>
                   <div class="col-md-2">
                     <a href="カート削除.php?name='.$row['bento_id'].'">
@@ -260,7 +260,7 @@
         </div>
         <div class="col-4">
           <?php
-            echo '<h2>'.$sum.'円</h2>';
+            echo '<h2>'.number_format($sum).'円</h2>';
           ?>
         </div>
         <div class="col-4">
@@ -279,6 +279,9 @@
       <div class="row">
         <div class="col-12">
           <h2>合計</h2>
+          <?php
+            echo '<h2>'.number_format($sum).'円</h2>';
+          ?>
         </div>
         <div class="col-12">
           <div class=text-center>
